f93bcd085d31f9e9c85470cab93376ca9dae11c1
Michael Klatt <mdklatt@alumni.ou.edu>
2019-10-15 00:59:17 -0500

    Fixed tests for pyenv scenario

--- molecule/pyenv/tests/test_default.py
+++ molecule/pyenv/tests/test_default.py
@@ -15,13 +15,22 @@ runner = AnsibleRunner(environ["MOLECULE_INVENTORY_FILE"])
 testinfra_hosts = runner.get_hosts("all")
 
 
-@pytest.mark.parametrize("cmd", ("python3.6", "python3", "pip3", "virtualenv"))
-def test_commands(host, cmd):
-    """ Test for installed Python commands.
+@pytest.fixture(scope="module")
+def pyenv(host):
+    """ Enable the pyenv Python.
 
     """
-    # FIXME: `pipenv` fails due to missing environment variables.
-    # FIXME: Fails when running as root: no pyenv installation found
-    for command in ("pyenv local 3.6.4", f"{cmd:s} --version"):
-        assert host.command(command).rc == 0
+    # Can't rely on PATH here because Testinfra seems to be using /bin/sh for
+    # execution, so .bash_profile is not being sourced correctly.
+    host.run("~/.pyenv/bin/pyenv local 3.6.4")
+    return
+
+
+@pytest.mark.usefixtures("pyenv")
+def test_python(host):
+    """ Test the installed Python.
+
+    """
+    python = "~/.pyenv/shims/python"  # path must be explicit
+    assert host.check_output(f"{python:s} --version") == "Python 3.6.4"
     return
